6698f5be0543f2b2ad7a58e245fe92be4151a5d0
sitaratoon <ajay393798@gmail.com>
2024-11-17 08:48:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/How_to_download_ST") # shareus ka tut_vid he 
 
 #force sub channel id, if you want enable force sub
-#FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
